2e857876910353c01d41b1887d939fb1a6e79ae3
keka3avr <kekazavr228@gmail.com>
2021-06-21 17:57:12 +1100

    code fixed, extra files deleted

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,1 @@
-// import * as things from './first.js';
-
-// import {months as m, User} from './first.js';
-// console.log(m);
-// const user = new User("Gusb");
-
-// console.log(user.name);
-
-// import Guk from './second.js';
-// const guk = new Guk("Chuk");
-// console.log(guk);
-
 (() => console.log('app worked'))();
--- src/js/second.js
+++
@@ -1,5 +0,0 @@
-export default class Guk {
-    constructor(name) {
-        this.name = `${name} :)`;
-    }
-}
